d506972264b414c0add398049facad905e66a462
yass.dev26 <90159581+Yassdev26@users.noreply.github.com>
2023-07-15 22:37:52 +0100

    Update Reding_Files1.py

--- Reding_Files1.py
+++ Reding_Files1.py
@@ -1,3 +1,15 @@
+#  -----------------------------------------------
+#  ------- File Handling => Read File ------------
+#  -----------------------------------------------
+
+#  -----------------------------------------------
+#  ------- /1/ File Data Object  -----------------
+#  -----------------------------------------------
+print(myFile)
+print(myFile.name) 
+print(myFile.mode)
+print(myFile.encoding)
+
 My_Files = open("file","r") #file for mac / file.txt
 print(My_Files.read())
 My_Files.close() #close the file
