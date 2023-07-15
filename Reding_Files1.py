d50238f00074e78077a858a7b3b87fdf4a168350
yass.dev26 <90159581+Yassdev26@users.noreply.github.com>
2023-07-15 23:20:36 +0100

    Update Reding_Files1.py

--- Reding_Files1.py
+++ Reding_Files1.py
@@ -1,17 +1,47 @@
 #  -----------------------------------------------
-#  ------- File Handling => Read File ------------
-#  -----------------------------------------------
-
-#  -----------------------------------------------
 #  ------- /1/ File Data Object  -----------------
 #  -----------------------------------------------
 
-My_Files = open("file","r") #file for mac / file.txt
 
 print(My_Files)
 print(My_Files.name) 
 print(My_Files.mode)
 print(My_Files.encoding)
 
+#  -----------------------------------------------
+#  ------- /2/ File Handling => Read File ------------
+#  -----------------------------------------------
+
+My_Files = open("file","r") #file for mac / file.txt for windows
+
 print(My_Files.read())
+
+print(My_Files.readline(5))
+print(My_Files.readline())
+print(My_Files.readline())
+
+
+print(My_Files.readlines())
+print(My_Files.readlines(50))
+print(type(My_Files.readlines()))
+
+
+# => I will give you example about for loops with reading files
+for line in myFile:
+    print(line)
+    if line.startswith("Islam he's from egypt"):
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
 My_Files.close() #close the file
