8ceaea673dceb947ea8b911cf4597890a7f43e89
yass.dev26 <90159581+Yassdev26@users.noreply.github.com>
2023-07-17 10:51:18 +0100

    Update Reading&Write_files.py

--- Reading&Write_files.py
+++ Reading&Write_files.py
@@ -2,11 +2,11 @@
 #  ------- /1/ File Data Object  -----------------
 #  -----------------------------------------------
 
-
 print(My_Files)
 print(My_Files.name) 
 print(My_Files.mode)
 print(My_Files.encoding)
+
 
 #  ---------------------------------------------------------
 #  ---/ r / ---> /2/ File Handling => Read File ------------
@@ -15,12 +15,10 @@ print(My_Files.encoding)
 My_Files = open("file","r") #file for mac / file.txt for windows
 
 print(My_Files.read())
-
+#readline / readlines
 print(My_Files.readline(5))
 print(My_Files.readline())
 print(My_Files.readline())
-
-
 print(My_Files.readlines())
 print(My_Files.readlines(50))
 print(type(My_Files.readlines()))
@@ -32,37 +30,37 @@ for line in myFile:
     if line.startswith("Tomorrow I will go to Morocco"):
         break
 
+
 #r+ => You can read the file and also write to it ...
 #  ------------------------------------------------------------------------
-#  ---/ r+ / ---> /2/ File Handling => Read File & Write to it ------------
+#  ---/ r+ / ---> /3/ File Handling => Read File & Write to it ------------
 #  ------------------------------------------------------------------------
 #YOU CAN READ THE FILE AND ALSO WRITE TO IT ...
-
 
 My_Files = open("file","r+")
 print(My_Files.write("She is my mother."))
 
+
 #  --------------------------------------------------------------------------------------------------------------------
-#  ---/ w / ---> /2/ File Handling => Delete the things that were in a file & add the things you are going to write ---
+#  ---/ w / ---> /4/ File Handling => Delete the things that were in a file & add the things you are going to write ---
 #  --------------------------------------------------------------------------------------------------------------------
 #DELETE THE THINGS THAT WERE IN A FILE & ADD THE THINGS YOU ARE GOING TO WRITE ...
-
 
 My_Files = open("write","w")
 print(My_Files.write("She is my mother."))
 
 
 #  ---------------------------------------------------------------------------------------------------------------------
-#  ---/ w+ / ---> /2/ File Handling => Delete the things that were in a file & add the things you are going to write ---
+#  ---/ w+ / ---> /5/ File Handling => Delete the things that were in a file & add the things you are going to write ---
 #  ---------------------------------------------------------------------------------------------------------------------
 #The difference between w and w + is that in w + a file can be created for you if you do not write any existing file, meaning when you write inside a file that does not exist on a project, it can be added while maintaining the same properties of w
-
 
 My_Files = open("write","w+")
 print(My_Files.write("The cat curled up on the cozy blanket and purred contentedly."))
 
+
 #  ---------------------------------------------------------------------------------------------------
-#  ---/ a / ---> /2/ File Handling => You can write to a file without deleting the previous write ----
+#  ---/ a / ---> /6/ File Handling => You can write to a file without deleting the previous write ----
 #  ---------------------------------------------------------------------------------------------------
 #YOU CAN WRITE TO A FILE WITHOUT DELETING THE PREVIOUS WRITE ...
 
@@ -70,11 +68,14 @@ My_Files = open("write","a")
 print(My_Files.write("The sun rises in the east and sets in the west."))
 
 
-#  ---------------------------------------------------------------------------------------------------
-#  ---/ a+ / ---> /2/ File Handling => You can write to a file without deleting the previous write ----
-#  ---------------------------------------------------------------------------------------------------
+#  ----------------------------------------------------------------------------------------------------
+#  ---/ a+ / ---> /7/ File Handling => You can write to a file without deleting the previous write ----
+#  ----------------------------------------------------------------------------------------------------
 # a+ like  w / w+ / a / a+  ( You can write to a file without deleting the previous write ).
+
 My_Files = open("write","a")
 print(My_Files.write("I love the smell of freshly baked bread in the morning."))
 
-My_Files.close() #close the file
+
+
+My_Files.close() #Close The File
